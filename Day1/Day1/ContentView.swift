9d0f57079bb117f7a90c503582551f4f83cc4847
Satoshi Hattori <>
2023-06-25 00:11:09 +0900

    add: day1

---
+++ Day1/Day1/ContentView.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+import RealityKit
+
+struct ContentView: View {
+
+    var body: some View {
+        NavigationStack {
+            Text("Hello, visionOS!")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
