b7d22f3ec63130db4c026db2e686a6143b8c4366
Cova8bitdot <cova.tech.8bitdots@gmail.com>
2023-09-16 16:58:51 +0900

    [Bugfix] Update API on Xcode15b8

--- Day16/Day16/ARKitSessionManager.swift
+++ Day16/Day16/ARKitSessionManager.swift
@@ -36,7 +36,7 @@ class ARKitSessionManager: ObservableObject {
     }
 
     func reportDevicePose() {
-        if let pose = worldTracking.queryPose(atTimestamp: CACurrentMediaTime()) {
+        if let pose = worldTracking.queryDeviceAnchor(atTimestamp: CACurrentMediaTime()) {
             print("pose: \(pose)")
         } else {
             print("pose: nil")
@@ -44,9 +44,9 @@ class ARKitSessionManager: ObservableObject {
     }
 
     func getOriginFromDeviceTransform() -> simd_float4x4 {
-        guard let pose = worldTracking.queryPose(atTimestamp: CACurrentMediaTime()) else {
+        guard let pose = worldTracking.queryDeviceAnchor(atTimestamp: CACurrentMediaTime()) else {
             return simd_float4x4()
         }
-        return pose.originFromDeviceTransform
+        return pose.originFromAnchorTransform
     }
 }
