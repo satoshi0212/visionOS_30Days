d442e9b3d3e28bf4c104e08ac9b7359f828970c1
Eli Hartnett <elichartnett@icloud.com>
2023-10-22 18:02:02 -0400

    Use TextureResource.loadAsync (over TextureResource.load) to get remove warning

--- Day14/Day14/ImmersiveView.swift
+++ Day14/Day14/ImmersiveView.swift
@@ -1,9 +1,11 @@
 import SwiftUI
 import RealityKit
+import Combine
 
 struct ImmersiveView: View {
 
     @State private var imageName: String
+    @State var textureRequest: AnyCancellable?
 
     init(imageName: String) {
         self.imageName = imageName
@@ -14,17 +16,18 @@ struct ImmersiveView: View {
 
             let rootEntity = Entity()
 
-            guard let texture = try? TextureResource.load(named: imageName) else {
-                return
-            }
-            var material = UnlitMaterial()
-            material.color = .init(texture: .init(texture))
-            rootEntity.components.set(ModelComponent(
-                mesh: .generateSphere(radius: 1E3),
-                materials: [material]
-            ))
-            rootEntity.scale *= .init(x: -1, y: 1, z: 1)
-            rootEntity.transform.translation += SIMD3<Float>(0.0, 1.0, 0.0)
+             textureRequest = TextureResource.loadAsync(named: imageName).sink { (error) in
+                 print(error)
+             } receiveValue: { (texture) in
+                 var material = UnlitMaterial()
+                 material.color = .init(texture: .init(texture))
+                 rootEntity.components.set(ModelComponent(
+                     mesh: .generateSphere(radius: 1E3),
+                     materials: [material]
+                 ))
+                 rootEntity.scale *= .init(x: -1, y: 1, z: 1)
+                 rootEntity.transform.translation += SIMD3<Float>(0.0, 1.0, 0.0)
+             }
 
             content.add(rootEntity)
         }
