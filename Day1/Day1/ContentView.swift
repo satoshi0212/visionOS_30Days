ce4d8482155e680e8dae11ece8f042773bb29398
Eli Hartnett <elichartnett@icloud.com>
2023-10-22 17:59:19 -0400

    Remove unused NavigationStacks

--- Day1/Day1/ContentView.swift
+++ Day1/Day1/ContentView.swift
@@ -1,12 +1,9 @@
 import SwiftUI
-import RealityKit
 
 struct ContentView: View {
 
     var body: some View {
-        NavigationStack {
-            Text("Hello, visionOS!")
-        }
+        Text("Hello, visionOS!")
     }
 }
 
